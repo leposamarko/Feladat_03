fb15e0b1e76e37a2066539bef7701800359cd6c8
Márkó Dániel Leposa <leposa.marko.daniel@gmail.com>
2023-02-11 14:20:18 +0100

    Add project files.

---
+++ Feladat_03/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Feladat_03
+{
+    class Program
+    {
+        static List<csoport> csoportlisat = new List<csoport>();
+        static void Main(string[] args)
+        {
+            Beolvasas();
+            Megoldasok();
+            
+        }
+
+        static void Beolvasas()
+        {
+            string gyujto = "";
+            int fo = 0;
+            using (StreamReader sr = new StreamReader("feladat3.txt"))
+            {
+                while (sr.Peek() >= 0)
+                {
+                    string str = sr.ReadLine();
+                    if (str.Equals(""))
+                    {
+                        csoport cs = new csoport(gyujto, fo);
+                        csoportlisat.Add(cs);
+                        gyujto = "";
+                        fo = 0;
+                    }
+                    else if(sr.Peek() == -1)
+                    {
+                        csoport cs = new csoport(gyujto, fo);
+                        csoportlisat.Add(cs);
+                    }
+                    else
+                    {
+                        gyujto += str;
+                        fo++;
+                    }
+
+                }
+            }
+        }
+
+        static void Megoldasok()
+        {
+            int db = 0;
+            int mindigen = 0;
+            foreach (csoport item in csoportlisat)
+            {
+                mindigen = 0;
+                db = 0;
+                for (int i = 0; i < item.Pontok.Length; i++)
+                {
+                    if (item.Pontok[i] > 0)
+                        db++;
+                    if (item.Pontok[i] == item.fo)
+                        mindigen++;
+                    ;
+                }
+                Console.WriteLine(item.csoportszam + ". csoportban ennyien kérdésre válaszolt legalább egyvalaki: " + db);
+
+                Console.WriteLine("Továbbá a csoporton belül enyi válaszra adott mindenki igen válast: " + mindigen + "\n");
+
+
+            }
+        }
+    }
+}
---
+++ Feladat_03/csoport.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Feladat_03
+{
+    class csoport
+    {
+        private static int szamozas = 1;
+        
+        public int csoportszam { get; private set; }
+        public int fo { get; private  set; }
+        private int[] pontok = new int[26];
+        public int[] Pontok { get => pontok; }
+
+        public csoport(string s,int f)
+        {
+            char[] charArr = s.ToCharArray();
+            for (int i = 0; i < charArr.Length; i++)
+            {
+                char a = charArr[i];
+                if((int)a >= 97 && (int)a <= 122)
+                {
+                    pontok[((int)a - 97)]++;
+                }
+            }
+            csoportszam = szamozas;
+            szamozas++;
+            fo = f;
+        }
+    }
+}
